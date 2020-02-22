af317e5846883dc12a22daa260bdbb3c36ef9aba
Sergey B <sergeyvbo@outlook.com>
2020-02-22 16:49:32 +0300

    GUI part 3

--- chess/src/com/chess/engine/board/BoardUtils.java
+++ chess/src/com/chess/engine/board/BoardUtils.java
@@ -6,17 +6,14 @@ public abstract class BoardUtils {
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
 
+    public static final boolean[] FIRST_ROW = initRow(0);
     public static final boolean[] SECOND_ROW = initRow(8);
+    public static final boolean[] THIRD_ROW = initRow(16);
+    public static final boolean[] FOURTH_ROW = initRow(24);
+    public static final boolean[] FIFTH_ROW = initRow(32);
+    public static final boolean[] SIXTH_ROW = initRow(40);
     public static final boolean[] SEVENTH_ROW = initRow(48);
-
-    private static boolean[] initRow(int rowNumber) {
-        final boolean[] row = new boolean[NUM_TILES];
-        do {
-            row[rowNumber] = true;
-            rowNumber++;
-        } while (rowNumber % NUM_TILES_PER_ROW != 0);
-        return row;
-     }
+    public static final boolean[] EIGHTH_ROW = initRow(56);
 
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
@@ -34,6 +31,16 @@ public abstract class BoardUtils {
         return column;
 
     }
+
+    private static boolean[] initRow(int rowNumber) {
+        final boolean[] row = new boolean[NUM_TILES];
+        do {
+            row[rowNumber] = true;
+            rowNumber++;
+        } while (rowNumber % NUM_TILES_PER_ROW != 0);
+        return row;
+    }
+
 
     public static boolean isValidTileCoordinate(final int candidateDestinationCoordinate) {
         return  candidateDestinationCoordinate >=0 && candidateDestinationCoordinate < 64;
